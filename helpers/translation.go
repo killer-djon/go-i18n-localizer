54fe17a8b972266380db1eb56be66ab423e1fff8
Leshanu E <e.leshanu@bwagroup.ru>
2021-01-19 14:22:55 +0300

    Make create instance for translations

---
+++ helpers/translation.go
@@ -0,0 +1,65 @@
+package helpers
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+const (
+	CONFIG_FILE              = "/go/bin/config.json"
+	DEFAULT_LOCALE           = "ru"
+	DEFAULT_TRANSLATION_PATH = "translations"
+	DEFAULT_CONTEXT          = "messages"
+)
+
+// LocalizerConfig structure for configuration
+type LocalizerConfig struct {
+	Locale          string `json:"locale"`
+	TranslationPath string `json:"translation_path"`
+	DefaultContext  string `json:"default_context"`
+}
+
+// NewTranslation create localizer config instance
+// of the structure to implement
+func NewTranslation(configFile ...string) *LocalizerConfig {
+	return ParseJson(configFile...)
+}
+
+// ParseJson try to parse json config file
+// for implements configuration options
+func ParseJson(configFile ...string) *LocalizerConfig {
+	var cFile *string
+	if len(configFile) > 0 {
+		cFile = &configFile[0]
+	}
+
+	var localeConfig = LocalizerConfig{
+		Locale:          DEFAULT_LOCALE,
+		TranslationPath: DEFAULT_TRANSLATION_PATH,
+		DefaultContext:  DEFAULT_CONTEXT,
+	}
+
+	if cFile != nil {
+		jsonFile, err := os.Open(*cFile)
+
+		if err != nil {
+			log.Printf("Error when try to open json config file %v", err)
+			return nil
+		}
+
+		defer jsonFile.Close()
+
+
+		decoder := json.NewDecoder(jsonFile)
+		err = decoder.Decode(&localeConfig)
+
+		if err != nil {
+			log.Println("Can't decode json config file, check json config and try again", err)
+			return nil
+		}
+	}
+
+	return &localeConfig
+}
+
--- localizer.go
+++ localizer.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"go-i18n-localizer/helpers"
+	"log"
+)
+
+
+func main() {
+	//localizerConfig := helpers.ParseJson("./config.json")
+	//localizerConfig := &helpers.LocalizerConfig{
+	//	Locale:          "en",
+	//	TranslationPath: "translation",
+	//	DefaultContext:  "user",
+	//}
+
+	localizerConfig := helpers.NewTranslation("./config.json")
+
+	log.Println("Start localizer", localizerConfig)
+}
